22d45edecac204e254de696b2f4be71c1d4afda0
Zach Brown <bigcountry66789@gmail.com>
2025-12-13 21:41:46 -0600

    Added the code for the prject and the instructions

---
+++ Charcter_Input.py
@@ -0,0 +1,10 @@
+#Create a program that asks the user for their name and age
+#Then prints a greeting using that information
+#Then tells them how many years they will turn 100
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"Hello {name}, you are {age} years old.")
+
+years = 100 - age
+print(f"You will be 100 years old in {years}"

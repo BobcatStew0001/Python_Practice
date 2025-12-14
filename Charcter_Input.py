c99f9bc86a68a1693e5460fedd49c0292cdf6151
Zach Brown <bigcountry66789@gmail.com>
2025-12-13 21:59:35 -0600

    Moved comment to top

--- Charcter_Input.py
+++ Charcter_Input.py
@@ -1,3 +1,4 @@
+#From pythonpractice.org
 #Create a program that asks the user for their name and age
 #Then prints a greeting using that information
 #Then tells them how many years they will turn 100
@@ -8,7 +9,7 @@ print(f"Hello {name}, you are {age} years old.")
 
 year = 2025 - age + 100
 print (f"You will be 100 years old in {year}")
-#From pythonpractice.org
+
 
 years = 100 - age
 print (f"That means you will be 100 in {years}")

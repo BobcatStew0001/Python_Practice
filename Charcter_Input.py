fd39c5e752f35a852d81d5f6ed228df257bc4074
Zach Brown <bigcountry66789@gmail.com>
2025-12-13 21:57:09 -0600

    Updated the code and included notes and comments

--- Charcter_Input.py
+++ Charcter_Input.py
@@ -6,7 +6,9 @@ age = int(input("Enter your age: "))
 
 print(f"Hello {name}, you are {age} years old.")
 
-years = 100 - age
-print(f"You will be 100 years old in {years}"
-
+year = 2025 - age + 100
+print (f"You will be 100 years old in {year}")
 #From pythonpractice.org
+
+years = 100 - age
+print (f"That means you will be 100 in {years}")
